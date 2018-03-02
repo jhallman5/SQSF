9e088d5770f43d0a5600e69f6c97faa0efc53bff
John Hallman <jhallman5@gmail.com>
2018-03-01 16:23:53 -0800

    Adds Navbar header

---
+++ src/views/containers/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+export default class Nav extends React.Component {
+
+  render(){
+    return (
+      <Navbar>
+        <Navbar.Header>
+          SQSF
+        </Navbar.Header>
+      </Navbar>
+    )
+  }
+}
